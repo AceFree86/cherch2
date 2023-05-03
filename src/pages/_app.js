e0e494bd5ad1b670c4ff8ead1d344356fb6ea556
Ruslan Mordovanets <gifler@me.com>
2023-05-03 23:42:16 +0300

    add GoogleAnalytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import GoogleAnalytics from "@/components/helpers/GoogleAnalytics";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <GoogleAnalytics GoogId={process.env.NEXT_PUBLIC_GIDA} />
       <main className="bg-white w-full min-h-screen">
         <SessionProvider>
           <NavBar />
